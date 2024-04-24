177ce9c4f0cf5559649d162943ff1c26431d6161
nadroj22 <e0725839@u.nus.edu>
2024-04-24 22:09:50 +0800

    Update app.py

--- Web_App/app.py
+++ Web_App/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import time
+import pandas as pd
+import psycopg2
 
 # Set the title of the web app
 st.title('Singapore Job Market Insights')
@@ -9,10 +11,29 @@ st.write('Welcome to the Singapore Job Market Insights Web Application! Get pers
 
 st.divider()
 
+# Function for loading data from the database
+def load_data(load_file_path='test.csv'):
+    select_table_query = """SELECT * FROM consolidatedJobs"""
+    conn = psycopg2.connect(database="is3107JobsDB", user='is3107Postgres', password='JobsDBProject3107', host='is3107.postgres.database.azure.com', port='5432')
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute(select_table_query)
+    result = cursor.fetchall()
+    conn.close()
+    colnames = ['job_title', 'description', 'company', 'salary_range']
+    pd.DataFrame(result, columns=colnames).to_csv(load_file_path)
+    return pd.DataFrame(result, columns=colnames)
+
 # Function for displaying job recommendation page
 def display_job_recommendation_page():
     st.title('Job Recommendation')
     st.write('Input your resume details and receive personalised job recommendations.')
+
+    # Load data from the database
+    job_data = load_data()
+
+    # Display job data to the user
+    st.write(job_data)
 
     # For user to input resume details
     resume_details = st.text_area("Resume Details", key='resume_details_input')
